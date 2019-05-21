8c48839736ef6209f61c6ef39faa1f6744a00fbf
Taq Karim <taqqui.karim@gmail.com>
2019-05-21 03:27:11 -0400

    updated script

--- scripts/convert_to_nb.py
+++ scripts/convert_to_nb.py
@@ -14,7 +14,7 @@ def get_cell(line, block):
 
 def mrkdown_to_nb(body, filename):
     nb = new_notebook()
-    nb.cells.append(new_markdown_cell(f"[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/mottaquikarim/PYTH2/blob/master/{get_ipynb_path(filename)})"))
+    nb.cells.append(new_markdown_cell(f"""<a href="https://colab.research.google.com/github/mottaquikarim/PYTH2/blob/master/{get_ipynb_path(filename)}" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>"""))
     current_block = []
     for line in body:
         if line.startswith('```'):
